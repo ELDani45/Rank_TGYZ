3133e5782e0c1c98954a361877f2cb367b2b7799
Daniel Alejandro Chilito Giron <chilito45giron@gmail.com>
2026-01-12 19:02:40 -0500

    Iniciamos el proyecto y hacemos el index.html (con una clase)

---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# Importamos la herramineta { VIEW } para indicarle a django que vamos a hacer una calse que sera una vista.
+
+# Create your views here.
+
+
+class Home(TemplateView):
+    template_name = "index.html"
