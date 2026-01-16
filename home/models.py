20d2927c2f5f8f16477f43c9ada4fd6256227f85
Daniel Alejandro Chilito Giron <chilito45giron@gmail.com>
2026-01-15 22:56:53 -0500

    interfaz y modelo base, OK!

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Puntos(models.Model):
+    puntos_totales = models.IntegerField(default=0)
+
+    def mostrar_puntos(self):
+        return str(self.puntos_totales)
+
+    def ganar_puntos(self,  cantidad: int) -> int:
+        suna = self.puntos_totales + cantidad
+        return suna
