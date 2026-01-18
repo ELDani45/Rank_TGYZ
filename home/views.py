f5b4bdf7a82876bd1c9446237822cfa8e06edfc7
Daniel Alejandro Chilito Giron <chilito45giron@gmail.com>
2026-01-17 22:52:25 -0500

    IMpmente el login y le estilos

--- home/views.py
+++ home/views.py
@@ -6,14 +6,15 @@ from home.forms import SignIn
 # Create your views here.
 
 
-class Home(TemplateView):
+class Home(TemplateView):  # se coloca " TemplateView " para que django interprete que esta clase es una vista
     template_name = "index.html"
 
 
 class Login(TemplateView):
     template_name = "login.html"
 
-    def Signin(self, request):
-        form = SignIn()
-        if request.method == "GET":
-            return {"form": form}
+    # metodo interno que trae toda vista de clase basda en Django para definir el contexto que se va a enviar al template
+    def get_context_data(self, **kwargs):
+        contex = super().get_context_data(**kwargs)
+        contex["signin"] = SignIn()
+        return contex
