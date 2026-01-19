26c37a9109d4b52065eea432d854ca6cf803a19a
Daniel Alejandro Chilito Giron <chilito45giron@gmail.com>
2026-01-18 21:53:35 -0500

    Signed-off-by: Daniel Alejandro Chilito Giron <chilito45giron@gmail.com> formulario hecho a medias, pero es cosa de timepo, queda pendinete darle estilo

--- vistas_generic/forms.py
+++ vistas_generic/forms.py
@@ -1,6 +1,12 @@
 from vistas_generic.models import Author
 from django import forms
 
+# Para hace de un modelo un formulario:
+# 1 - primero debemos de importat el modelo que vamos a utilizar
+# 2 -luego de " forms " llamamos al comando de " ModelfForm "
+# 3 -Dentro de la clase creamos otra clase con eo nombre especial de " Meta "
+# 4 - Ahi dentro definimos la siguientes variiables: [ " model " y " fields " ]
+
 
 class MakeAuthor(forms.ModelForm):
     class Meta:
